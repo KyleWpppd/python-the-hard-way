a98883b99fd2949a4ff6b94e095b07976c19d550
Kyle Wanamaker <kyle@klowell.com>
2011-05-21 10:19:01 -0400

    Completed ex1 extra credit 1

---
+++ ex1/ex1-ec1.py
@@ -0,0 +1,17 @@
+"""
+Copyright
+Kyle Wanamaker
+May 21, 2011
+
+Exercise 1
+Learn Python the Hard Way
+"""
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "This is an 'extra' line."
