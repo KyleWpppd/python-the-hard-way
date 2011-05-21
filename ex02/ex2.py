e950177ed430c4bbfccd32aaaca0d5a651c4e7c6
Kyle Wanamaker <kyle@klowell.com>
2011-05-21 10:28:18 -0400

    Added example 2 folder and file to project

---
+++ ex02/ex2.py
@@ -0,0 +1,21 @@
+"""
+Copyright 
+Kyle Wanamaker 
+May 21, 2011
+
+Exercise 02
+Learn Python the Hard Way
+http://learnpythonthehardway.org/
+
+"""
+
+
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by python
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can use a comment to "disable" or comment out a piece of code. 
+# print "This won't run."
+
+print "This will run."
