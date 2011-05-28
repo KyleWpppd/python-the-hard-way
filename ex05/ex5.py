7d7e2e9fa566331fad90bef6c2ed707e42629cf7
Kyle Wanamaker <kyle@klowell.com>
2011-05-28 10:12:10 -0400

    Updated ex5.py for extra credit 5.4

--- ex05/ex5.py
+++ ex05/ex5.py
@@ -12,6 +12,10 @@ my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
+my_weight_kg = my_weight * 2.241
+my_height_cm = my_height * 2.54
+my_age_dog_yrs = my_age * 7
+
 print "Let's talk about %s." % my_name
 print "He's %d inches tall." % my_height
 print "He's %d pounds heavy." % my_weight
@@ -22,3 +26,6 @@ print "His teeth are usually %s depending on the coffee." % my_teeth
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I'd get %d." % (
 	my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+print "If I were a dog, I would be %d years old." % my_age_dog_yrs
+print "In Europe I would weigh %fkg and be %fcm tall." % (my_weight_kg, my_height_cm)
